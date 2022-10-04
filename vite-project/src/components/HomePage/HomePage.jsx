6fcdacb35deb918c2c9167016eefd052276d6adb
Chandrakishor Tiwari <chandrkishor@mindrops.com>
2022-10-04 06:56:05 +0530

    home page structure

--- vite-project/src/components/HomePage/HomePage.jsx
+++ vite-project/src/components/HomePage/HomePage.jsx
@@ -1,15 +1,21 @@
-import { AlertTitle, Grid } from "@mui/material";
+import { AlertTitle, Box, Grid } from "@mui/material";
 import React from "react";
-import Miniview from "./Miniview/Miniview";
+import MiniView from "./MiniView/MiniView";
+import data from "./data.json";
 
 const HomePage = () => {
+  const { articles } = data;
   return (
     <Grid sx={{ mt: "65px" }} container>
+      <h3>Home pages</h3>
       <Grid>
-        <AlertTitle>Home pages</AlertTitle>
-      </Grid>
-      <Grid>
-        <Miniview />
+        {articles.map((item) => {
+          return (
+            <Box sx={{ mb: 1 }}>
+              <MiniView key={item._id} data={item} />
+            </Box>
+          );
+        })}
       </Grid>
     </Grid>
   );
--- vite-project/src/components/HomePage/Miniview/Miniview.jsx
+++ vite-project/src/components/HomePage/Miniview/Miniview.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const Miniview = () => {
-  return <div>Miniview</div>;
+const MiniView = ({ data = {} }) => {
+  const { title, para } = data;
+  console.log(title, para);
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="body2">{para}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">View full article</Button>
+      </CardActions>
+    </Card>
+  );
 };
-
-export default Miniview;
+export default MiniView;
